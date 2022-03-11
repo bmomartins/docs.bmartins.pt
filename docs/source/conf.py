51d58f0c8c1b06c3eb5aed340d813f3b513c2efd
Bruno Martins <bruno@bmartins.pt>
2022-03-11 14:46:34 +0000

    minor changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,11 +3,11 @@
 # -- Project information
 
 project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2022, Bruno Martins'
+author = 'Bruno Martins'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.1.1'
 
 # -- General configuration
 
